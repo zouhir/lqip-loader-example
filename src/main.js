d74a5b1d64c3f1a023e07720eb14fba2721f317d
Zouhir <zouhir@zouhir.org>
2017-05-25 21:06:35 +1000

    initial commit

---
+++ src/main.js
@@ -0,0 +1,28 @@
+require('./style.scss');
+
+
+var image1 = require('./media/mount.jpg');
+var image2 = require('./media/bee.jpg');
+var image3 = require('./media/lamps.jpg');
+var image4 = require('./media/paper-ships.jpg');
+
+(function() {
+
+  var root = document.getElementById("gallery");
+  var images = [image1, image2, image3, image4];
+
+  images.forEach(function(img){
+
+    console.log(img)
+    root.innerHTML += template(img.preSrc, img.src)
+  })
+
+  function template(preSrc, src) {
+    return `
+      <section>
+        <img src='${preSrc}' />
+        <img src='${src}' />
+      </section>
+    `};
+
+}());
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  context: path.resolve('src'),
+  entry: './main.js',
+  output: {
+    path: path.resolve('dist'),
+    publicPath: '/',
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        loaders: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader',
+            {
+              loader: 'postcss-loader',
+              options: {
+                plugins: function() {
+                  return [
+                    require('autoprefixer')({ browsers: ['> 1%', 'IE >= 10'] })
+                  ];
+                }
+              }
+            },
+            {
+                loader: "sass-loader",
+                options: {
+                    sourceMap: true
+                }
+            }
+          ]
+        })
+      },
+      {
+        test: /\.(jpe?g)(\?.*)?$/i,
+        loaders: [
+          {
+              loader: 'lqip-loader',
+              options: {
+                
+                path: '/media'
+              }
+            },
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: '[name].css'
+    }),
+    new CopyWebpackPlugin([
+      { from: 'favicons'},
+      { from: 'media', to: 'media' }
+    ])
+  ]
+};

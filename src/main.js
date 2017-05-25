e824a712a2d12acba2ce0940e557a6983443d613
Zouhir <zouhir@zouhir.org>
2017-05-25 22:47:13 +1000

    demoing cut-over

--- src/main.js
+++ src/main.js
@@ -1,21 +1,22 @@
-require('./style.scss');
+require("./style.scss");
 
-
-var image1 = require('./media/mount.jpg');
-var image2 = require('./media/bee.jpg');
-var image3 = require('./media/lamps.jpg');
-var image4 = require('./media/paper-ships.jpg');
+var image1 = require("./media/mount.jpg");
+var image2 = require("./media/bee.jpg");
+var image3 = require("./media/lamps.jpg");
+var image4 = require("./media/paper-ships.jpg");
 
 (function() {
-
   var root = document.getElementById("gallery");
   var images = [image1, image2, image3, image4];
 
-  images.forEach(function(img){
-
-    console.log(img)
-    root.innerHTML += template(img.preSrc, img.src)
-  })
+  images.forEach(function(image) {
+    var img = document.createElement("img");
+    img.src = image.preSrc;
+    root.appendChild(img);
+    setTimeout(function() {
+      img.src = image.src;
+    }, Math.floor(Math.random() * (1500 - 500 + 1) + 500));
+  });
 
   function template(preSrc, src) {
     return `
@@ -23,6 +24,7 @@ var image4 = require('./media/paper-ships.jpg');
         <img src='${preSrc}' />
         <img src='${src}' />
       </section>
-    `};
+    `;
+  }
 
-}());
+})();
